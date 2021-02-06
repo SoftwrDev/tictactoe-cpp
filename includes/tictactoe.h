f2dcd7bc618ae2196921e3c6417deb4839a0aa1f
SoftwrDev <contato.santosgabriel@gmail.com>
2021-02-06 11:36:57 -0300

    chore: revert changes on didGameEnd

--- includes/tictactoe.h
+++ includes/tictactoe.h
@@ -27,22 +27,21 @@ namespace TicTacToe {
             PlayOptions winner;          
               
             bool didGameEnd() {
-                bool didWin = 
-                    (board[0][0] != BOARD_EMPTY && board[0][0] == board[0][1] && board[0][1] == board[0][2]) ||
-                    (board[1][0] != BOARD_EMPTY && board[1][0] == board[1][1] && board[1][1] == board[1][2]) ||
-                    (board[2][0] != BOARD_EMPTY && board[2][0] == board[2][1] && board[2][1] == board[2][2]) ||
-                    (board[0][0] != BOARD_EMPTY && board[0][0] == board[1][0] && board[1][0] == board[2][0]) ||
-                    (board[0][1] != BOARD_EMPTY && board[0][1] == board[1][1] && board[1][1] == board[2][1]) ||
-                    (board[0][2] != BOARD_EMPTY && board[0][2] == board[1][2] && board[1][2] == board[2][2]) ||
-                    (board[0][0] != BOARD_EMPTY && board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
-                    (board[0][2] != BOARD_EMPTY && board[0][2] == board[1][1] && board[1][1] == board[2][0]);
-
-                if(didWin) {
+                if((board[0][0] != BOARD_EMPTY && board[0][0] == board[0][1] && board[0][1] == board[0][2]) 
+                || (board[1][0] != BOARD_EMPTY && board[1][0] == board[1][1]  && board[1][1] == board[1][2])
+                || (board[2][0] != BOARD_EMPTY && board[2][0] == board[2][1]  && board[2][1] == board[2][2])
+                
+                || (board[0][0] != BOARD_EMPTY && board[0][0] == board[1][0] && board[1][0] == board[2][0])
+                || (board[0][1] != BOARD_EMPTY && board[0][1] == board[1][1] && board[1][1] == board[2][1])
+                || (board[0][2] != BOARD_EMPTY && board[0][2] == board[1][2] && board[1][2] == board[2][2])
+                
+                || (board[0][0] != BOARD_EMPTY && board[0][0] == board[1][1] && board[1][1] == board[2][2])
+                || (board[0][2] != BOARD_EMPTY && board[0][2] == board[1][1] && board[1][1] == board[2][0])) 
+                {
                     playing = false;
                     winner = turn;
                     return true;
                 }
-                
                 return false;
             }
 
